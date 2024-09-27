427940c7423ee57c4fcbaf58a094b3a774f79b07
Роман Назмиев <roma.nazmiev@gmail.com>
2024-09-27 17:52:17 +0700

    docs add

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Flow Chart Demo',
+      title: 'SiberSuite App',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
